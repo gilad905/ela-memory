a3c8e9dd58c205bb1803d436c200eca214b80868
Gilad Mayani <gilad905@gmail.com>
2025-04-11 10:59:23 +0300

    .

--- @other/zoomer.ps1
+++ @other/zoomer.ps1
@@ -1,19 +1,17 @@
 Push-Location .\@raw-assets\images
 
-$input_img = "WhatsApp Image 2025-04-03 at 13.52.04.jpeg"
-$output = "zoomed./12.jpeg"
-
+$number = "34"
 $gravity = "Center"
-$cropWidthPercent = 65
-$cropHeightPercent = 65
+$percent = 65
 
+$input_img = "$number.jpeg"
+$output = "../zoomed/$number.jpeg"
 $dimensions = magick identify -format "%w %h" $input_img
 $parts = $dimensions -split " "
 $width = [int]$parts[0]
 $height = [int]$parts[1]
-
-$cropWidth = [math]::Round($width * $cropWidthPercent / 100)
-$cropHeight = [math]::Round($height * $cropHeightPercent / 100)
+$cropWidth = [math]::Round($width * $percent / 100)
+$cropHeight = [math]::Round($height * $percent / 100)
 
 magick $input_img -gravity $gravity -crop "${cropWidth}x${cropHeight}+0+0" +repage $output
 
